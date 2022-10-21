c11c9724585755a52dd78aa52c3a96b72016da00
wigarwijaya <wigarwijaya@gmail.com>
2022-10-21 16:48:35 +0700

    hashing and salting password using bcrypt

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const port = 3000;
 
 const app = express();
-
-console.log(md5("12345"));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/userDB');
+mongoose.connect("mongodb://localhost:27017/userDB");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    email: String,
-    password: String
+  email: String,
+  password: String,
 });
 
-
 const User = mongoose.model("User", userSchema);
-
 
 // --------------------------------------------------------------------------
 app.get("/", (req, res) => {
@@ -39,36 +36,38 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", (req, res) =>{
+app.post("/register", (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
     const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+      email: req.body.username,
+      password: hash,
     });
-    newUser.save()
-    .then(() => res.render("secrets"))
-    .catch(() => console.log(err));
+    newUser
+      .save()
+      .then(() => res.render("secrets"))
+      .catch(() => console.log(err));
+  });
 });
 
-app.post("/login", (req, res) =>{
+app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({ email: username })
-//   .then((data) => {
-//     res.status(200).json({
-//         text: "User found",
-//         data: data
-//     });
-// })
-  .then(foundUser => {
-    if (foundUser.password === password){
-      res.render("secrets")
-    }
-  })
-  .catch((err) => console.log(err))
+    //   .then((data) => {
+    //     res.status(200).json({
+    //         text: "User found",
+    //         data: data
+    //     });
+    // })
+    .then((foundUser) => {
+      // Load hash from your password DB.
+      bcrypt.compare(password, foundUser.password)
+      .then((result) => res.render("secrets", console.log("Login Successfully!")))
+      .catch(() => console.log("login failed"));
+    })
+    .catch((err) => console.log(err));
 });
-
-
 
 // --------------------------------------------------------------------------
 app.listen(port, (req, res) => {
